07a110a61f7dba78c28efbacf6a4ec1064e8ff74
Jack Champagne <jackchampagne.r@gmail.com>
2021-07-02 09:35:39 -0400

    Add normal convolution structure

--- z2_conv.py
+++ z2_conv.py
@@ -5,10 +5,10 @@ import torch.nn.functional as F
 class ConvNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 184)
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16 * 6 * 6, 184)
         self.fc2 = nn.Linear(184, 64)
         self.fc3 = nn.Linear(64, 10)
 
