9e0aadcd031440b73de5e7dd0caccc9fd801cf1d
Jack Champagne <jackchampagne.r@gmail.com>
2021-07-02 01:50:05 -0400

    Add p4 conv structure

---
+++ p4_conv.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import gconvutils as gutils
+from groupy.gconv.pytorch_gconv import P4MConvZ2, P4MConvP4M
+
+# TODO: Seperate the following into a normal convolution file
+        # self.conv1 = nn.Conv2d(1, 6, 5)
+        # self.pool = nn.MaxPool2d(2, 2) - getting replaced by plane_group_max_pooling
+        # self.conv2 = nn.Conv2d(6, 16, 5)
+
+class P4Net(nn.Module):
+    def __init__(self):
+        super(P4Net, self).__init__() 
+        self.conv1 = P4MConvZ2(in_channels=1, out_channels=8, kernel_size=5, stride=1)
+        self.conv2 = P4MConvP4M(in_channels=8, out_channels=16, kernel_size=5, stride=1)
+        self.conv3 = P4MConvP4M(in_channels=16, out_channels=32, kernel_size=5)
+
+        self.fc1 = nn.Linear(32, 16)
+        self.fc2 = nn.Linear(16, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = gutils.plane_group_spatial_max_pooling(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = gutils.plane_group_spatial_max_pooling(x, 2, 2)
+        x = F.relu(self.conv3(x))
+        x = gutils.plane_group_spatial_max_pooling(x, 1, 1)
+        x = torch.max(x, dim=2)[0]
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
